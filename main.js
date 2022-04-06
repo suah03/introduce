5c09f3d00b777b23dbab6761099d7759ee337aae
suah03 <1128smil@gmail.com>
2022-04-06 19:43:00 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    b1.onclick = function() {
+        name = prompt("이름을 입력하세요.");
+        document.getElementsByClassName("name")[0].innerHTML = "name : " + name;
+
+    };
+    b2.onclick = function() {
+        name = prompt("전화번호를 입력하세요.");
+        document.getElementsByClassName("number")[0].innerHTML = "number : " + Number;
+
+    };
+    b3.onclick = function() {
+        name = prompt("이름을 입력하세요.");
+        document.getElementsByClassName("age")[0].innerHTML = "age : " + name;
+
+    };
+    b4.onclick = function() {
+        name = prompt("이름을 입력하세요.");
+        document.getElementsByClassName("name")[0].innerHTML = "name : " + name;
+
+    };
+    b5.onclick = function() {
+        name = prompt("이름을 입력하세요.");
+        document.getElementsByClassName("name")[0].innerHTML = "name : " + name;
+
+    };
+    
+};
